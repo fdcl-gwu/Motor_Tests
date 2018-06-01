30f72979359d18187fa393e7098ae4520d1f82e8
Mahdis Bisheban <mbshbn@gwmail.gwu.edu>
2018-06-01 13:32:59 -0400

    addd the final figure of C_tau

--- tiger_5_31_2018/compute C_tau/compute_C_tau.m
+++ tiger_5_31_2018/compute C_tau/compute_C_tau.m
@@ -11,42 +11,45 @@ plot(Torque_thrust_function,'--',Thrust_avg,Torque_avg','k.');
 xlabel('Thrust avg (N)')
 ylabel('Torque avg (Nm)')
 
-font_f=16;
-figure
-scatter(Command_avg,Thrust_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex')
-saveas(gcf,[pwd ['C_tau_148thrust' ],'.fig']);
-saveas(gcf,[pwd ['C_tau_148thrust' ],'.eps'],'epsc2');
-
-figure
-scatter(Command_avg,Torque_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Torque_avg (Nm)','fontsize',font_f,'interpreter','latex')
-saveas(gcf,[pwd ['C_tau_148torque' ],'.fig']);
-saveas(gcf,[pwd ['C_tau_148torque' ],'.eps'],'epsc2');
-
-figure
-scatter(Thrust_avg,Torque_avg)
-xlabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex');
-ylabel('Torque_avg (Nm)','fontsize',font_f,'interpreter','latex')
-saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.fig']);
-saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.eps'],'epsc2');
-
-
-figure
-scatter(Command_avg,Torque_avg./Thrust_avg)
-xlabel('Command_avg ','fontsize',font_f,'interpreter','latex');
-ylabel('Torque_avg/Thrust_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.fig']);
-saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.eps'],'epsc2');
-
 %% Data from motor manual:
 T_avg_manual=[4.508;6.958;8.526;10.584;12.054];
 Torque_avg_manual=[0.08;0.13;0.15;0.19;0.21];
 Torque_thrust_function_manual = fit(T_avg_manual,Torque_avg_manual,'poly1')
-figure
-plot(Torque_thrust_function_manual,'--',T_avg_manual,Torque_avg_manual','k.');
+%figure
+hold on
+plot(Torque_thrust_function_manual,'-- r.',T_avg_manual,Torque_avg_manual','r.');
 xlabel('Thrust avg (N)')
 ylabel('Torque avg (Nm)')
-title('From motor manual')
+legend('Test data','Test fit','Manual data','Manual fit')
+%title('From motor manual')
+%% Plot for mour test
+% font_f=16;
+% figure
+% scatter(Command_avg,Thrust_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,[pwd ['C_tau_148thrust' ],'.fig']);
+% saveas(gcf,[pwd ['C_tau_148thrust' ],'.eps'],'epsc2');
+% 
+% figure
+% scatter(Command_avg,Torque_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Torque_avg (Nm)','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,[pwd ['C_tau_148torque' ],'.fig']);
+% saveas(gcf,[pwd ['C_tau_148torque' ],'.eps'],'epsc2');
+% 
+% figure
+% scatter(Thrust_avg,Torque_avg)
+% xlabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex');
+% ylabel('Torque_avg (Nm)','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.fig']);
+% saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.eps'],'epsc2');
+% 
+% 
+% figure
+% scatter(Command_avg,Torque_avg./Thrust_avg)
+% xlabel('Command_avg ','fontsize',font_f,'interpreter','latex');
+% ylabel('Torque_avg/Thrust_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.fig']);
+% saveas(gcf,[pwd ['C_tau_148torquethrust' ],'.eps'],'epsc2');
+
