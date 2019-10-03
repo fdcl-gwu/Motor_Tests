ea1f63061f93db7c5feeb42bf3396008a7a150d2
Kani-FDCL <kanishkegb@gwu.edu>
2019-10-03 19:04:23 -0400

    clean post processing file

--- Post_process_Matlab_files/post_porcess_thrust_test.m
+++ Post_process_Matlab_files/post_porcess_thrust_test.m
@@ -1,102 +1,55 @@
 clear;close all;clc
-%% Import text data:
-% First import each text file as a numeric matrix. In matlab set "Output
-% Type" as "Numeric Matrix", shown in the figure.
 
-% Or use the following:
-calib0 = import_text_data("calib_0.txt");
-calib72 = import_text_data("calib_72.txt");
+path = "../tiger_u3_apc10x4p7/";
+motor_calibration_file = path + 'motor_calib_16p4.txt';
 
-calib100 = import_text_data("calib_100.txt");
-calib110 = import_text_data("calib_110.txt");
-calib120 = import_text_data("calib_120.txt");
-calib172 = import_text_data("calib_172.txt");
+y=[0; 72; 100; 172; 500; 600];  % W2 (gram) on the sensor
 
-calib200 = import_text_data("calib_200.txt");
-calib220 = import_text_data("calib_220.txt");
-calib272 = import_text_data("calib_272.txt");
+calib0 = import_text_data(path + "calib_0.txt");
+calib72 = import_text_data(path + "calib_72.txt");
+calib100 = import_text_data(path + "calib_100.txt");
+calib172 = import_text_data(path + "calib_172.txt");
+calib500 = import_text_data(path + "calib_500.txt");
+calib600 = import_text_data(path + "calib_600.txt");
 
-calib300 = import_text_data("calib_300.txt");
-calib320 = import_text_data("calib_320.txt");
-calib372 = import_text_data("calib_372.txt");
 
-calib400 = import_text_data("calib_400.txt");
-calib410 = import_text_data("calib_410.txt");
-calib420 = import_text_data("calib_420.txt");
-calib440 = import_text_data("calib_440.txt");
-calib450 = import_text_data("calib_450.txt");
-calib472 = import_text_data("calib_472.txt");
+% length of the arm from pivot to the motor in meters
+arm_motor = 8.5 / 100;
 
-calib500 = import_text_data("calib_500.txt");
-calib510 = import_text_data("calib_510.txt");
-calib520 = import_text_data("calib_520.txt");
-calib540 = import_text_data("calib_540.txt");
-calib572 = import_text_data("calib_572.txt");
-calib592 = import_text_data("calib_592.txt");
+% length of the arm from pivot to the load cell in meters
+arm_load_cell = 23 / 100;  
 
-calib620 = import_text_data("calib_620.txt");
 
 save all_calib_thrust_data
-%% reading calibration data:
+
+%% Reading calibration data:
+
 % load all_calib_thrust_data
-% Do not use the first and the last parts of data 
 x(1)=mean(keep_last_2000(calib0));
 x(2)=mean(keep_last_2000(calib72));
+x(3)=mean(keep_last_2000(calib100));
+x(4)=mean(keep_last_2000(calib172));
+x(5)=mean(keep_last_2000(calib500));
+x(6)=mean(keep_last_2000(calib600));
 
-x(3)=mean(keep_last_2000(calib100));%%%
-x(4)=mean(keep_last_2000(calib110));%%%
-x(5)=mean(keep_last_2000(calib120));%%%
-x(6)=mean(keep_last_2000(calib172));%%%
+%% Meter Calibration
+% Find the relationship between reading from load cell and weights
 
-x(7)=mean(keep_last_2000(calib200));
-x(8)=mean(keep_last_2000(calib220));
-x(9)=mean(keep_last_2000(calib272));
+mass_function = fit(x', y, 'poly1')
 
-x(10)=mean(keep_last_2000(calib300));
-x(11)=mean(keep_last_2000(calib320));
-x(12)=mean(keep_last_2000(calib372));
-
-x(13)=mean(keep_last_2000(calib400));
-x(14)=mean(keep_last_2000(calib410));
-x(15)=mean(keep_last_2000(calib420));
-x(16)=mean(keep_last_2000(calib440));
-x(17)=mean(keep_last_2000(calib450));
-x(18)=mean(keep_last_2000(calib472));
-
-x(19)=mean(keep_last_2000(calib500));
-x(20)=mean(keep_last_2000(calib510));
-x(21)=mean(keep_last_2000(calib520));
-x(22)=mean(keep_last_2000(calib540));
-x(23)=mean(keep_last_2000(calib572));
-x(24)=mean(keep_last_2000(calib592));
-
-x(25)=mean(keep_last_2000(calib620));
-
-%% Find the relationship between reading from load cell and weights
-x % reading from load cell
-y=[0;72; 100;110;120;172; 200;220;272; 300;320;372; 400;410;420;440;450;472;];% 500;510;520;540;572;592; 620];% known mass (gram) on the sensor
-% for this 5 masses reading is the same, so do not use them!
-size(y)
-mass_function = fit(x(1:18)',y,'poly1')
-% mass_function = 
-%      Linear model Poly1:
-%      mass_function(x) = p1*x + p2
-%      Coefficients (with 95% confidence bounds):
-%  p1 =      0.5419  (0.4979, 0.5858)
-%        p2 =      -34.53  (-68.64, -0.4185)
 figure
-plot(mass_function,'--',x(1:18),y','k.');
-xlabel('Reading from load cell')
-ylabel('Mass (gr)')
-%%
-arm_motor=8.5/100;
-arm_load_cell=23/100;
-%% Read File
-filename = 'Motor_16p3.txt'%F:\FDCL Mahdis Git\Motor_Tests\tiger7_2_2018_shorter_wire\
+plot(x, mass_function(x), '--', x, y', 'k.');
+title('Meter Calibration');
+xlabel('Meter Reading');
+ylabel('Mass (grams)');
+
+%% Motor Calibration
+
 delimiter = ',';
 formatSpec = '%f%f%f%f%f%f%f%f%[^\n\r]';
-fileID = fopen(filename,'r');
-dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+fileID = fopen(motor_calibration_file,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, ...
+    'EmptyValue' ,NaN, 'ReturnOnError', false);
 fclose(fileID);
 
 Command = dataArray{:, 1};     % 10 : 250
@@ -107,143 +60,133 @@ RPM_Hz = dataArray{:, 5};
 MAH = dataArray{:, 6};
 Volt = dataArray{:, 7};
 Omega_read = dataArray{:, 8};
+
 clearvars filename delimiter formatSpec fileID dataArray ans;
-%% Variable scalings
-Current=Current/10;
-Volt=Volt./10;
-RPM=RPM_Hz.*60;
 
-Weight=9.8*mass_function(Omega_read)/1000;% kg
-thrust=arm_load_cell.*Weight/arm_motor;% N
-%%
-% cell arrays:
-for j=1:22
-    A{j} =find(Command==10+(j-1)*10);
-    Command_avg{j}=10+(j-1)*10;
-    init_state{j}=find(State(A{j})==255,1)+150;% A{j}(1)+% 150=350*3/7, 350=7700/21
-                                               % do not use 3 first seconds of data
-    Current_avg{j}=mean(Current(A{j}(init_state{j}:end)));
-    Temp_avg{j}=mean(Temp(A{j}(init_state{j}:end)));
-    RPM_Hz_avg{j}=mean(RPM_Hz(A{j}(init_state{j}:end)));
-    RPM_avg{j}=mean(RPM(A{j}(init_state{j}:end)));
-    MAH_avg{j}=mean(MAH(A{j}(init_state{j}:end)));
-    Volt_avg{j}=mean(Volt(A{j}(init_state{j}:end)));
-    Weight_avg{j}=mean(Weight(A{j}(init_state{j}:end)));
+% Variable scalings
+Current = Current / 10;
+Volt = Volt ./ 10;
+RPM = RPM_Hz .* 60;
 
-    Thrust_avg{j}=mean(thrust(A{j}(init_state{j}:end)));
+Weight= 9.8 * mass_function(Omega_read) / 1000;  % kg
+thrust = arm_load_cell .* Weight / arm_motor;  % N
+
+for j = 1:25  % Arduino sends 10:10:250
+    A{j} = find(Command == 10 + (j - 1) * 10);
+    Command_avg{j} = 10 + (j - 1) * 10;
+    init_state{j} = find(State(A{j}) == 255, 1) + 150;
+    Current_avg{j} = mean(Current(A{j}(init_state{j}:end)));
+    Temp_avg{j} = mean(Temp(A{j}(init_state{j}:end)));
+    RPM_Hz_avg{j} = mean(RPM_Hz(A{j}(init_state{j}:end)));
+    RPM_avg{j} = mean(RPM(A{j}(init_state{j}:end)));
+    MAH_avg{j} = mean(MAH(A{j}(init_state{j}:end)));
+    Volt_avg{j} = mean(Volt(A{j}(init_state{j}:end)));
+    Weight_avg{j} = mean(Weight(A{j}(init_state{j}:end)));
+
+    Thrust_avg{j} = mean(thrust(A{j}(init_state{j}:end)));
     %Torque_avg{j}=mean(torque(A{j}(init_state{j}:end)));
 end
-Command_avg=cell2mat(Command_avg);
-init_state=cell2mat(init_state);
-Current_avg=cell2mat(Current_avg);
-Temp_avg=cell2mat(Temp_avg);
-RPM_Hz_avg=cell2mat(RPM_Hz_avg);
-RPM_avg=cell2mat(RPM_avg);
-MAH_avg=cell2mat(MAH_avg);
-Volt_avg=cell2mat(Volt_avg);
-Weight_avg=cell2mat(Weight_avg);
-Thrust_avg=cell2mat(Thrust_avg);
 
-Thrust_from_throttle_func = fit(Command_avg',Thrust_avg','poly2')
-% Linear model Poly2:
-% if p3 forced to set to 0, to have 0thrust from throttle:
-% Coefficients (with 95% confidence bounds):
-%        p1 =   0.0001604  (0.000142, 0.0001787)
-%        p2 =     0.01528  (0.01209, 0.01848)
-%        p3 =           0  (fixed at bound)
+Command_avg = cell2mat(Command_avg);
+init_state = cell2mat(init_state);
+Current_avg = cell2mat(Current_avg);
+Temp_avg = cell2mat(Temp_avg);
+RPM_Hz_avg = cell2mat(RPM_Hz_avg);
+RPM_avg = cell2mat(RPM_avg);
+MAH_avg = cell2mat(MAH_avg);
+Volt_avg = cell2mat(Volt_avg);
+Weight_avg = cell2mat(Weight_avg);
+Thrust_avg = cell2mat(Thrust_avg);
 
-%OR, let Matlab fit the best with no constraint on p3:
-%      f(x) = p1*x^2 + p2*x + p3
-% Coefficients (with 95% confidence bounds):
-       p1 =   0.0002036 ;% (0.0001897, 0.0002175)
-       p2 =    0.003627 ;% (0.0003335, 0.00692)
-       p3 =      0.6563 ;% (0.4919, 0.8207)
+
+calibration_curve = fit(Thrust_avg', Command_avg', 'poly2')
 
 figure
-plot(Thrust_from_throttle_func,'--',Command_avg,Thrust_avg,'k.');
-ylabel('Thrust (N)')
-xlabel('Throttle')
-title('Thrust=p1*throttle^2 + p2*throttle + p3')
-
-% Now find the inverse of 2nd order polynomial:
-Command_avg_inversepoly2=sqrt((Thrust_avg-p3)/p1+p2^2/4/p1/p1)-p2/2/p1;
-
-figure
-plot(Thrust_avg,Command_avg_inversepoly2,'--',Thrust_avg,Command_avg,'k.');
+plot(Thrust_avg, calibration_curve(Thrust_avg), '--', ...
+    Thrust_avg, Command_avg, 'k.');
 xlabel('Thrust (N)')
-ylabel('Throttle')
-title('throttle=sqrt((Thrust-p3)/p1+p2^2/4/p1/p1)-p2/2/p1')
+ylabel('Throttle Command')
+title('Motor Calibration Curve')
 
-%>>>>>>>>>>>>>use "cftool" for the curve fitting <<<<<< 
-% from cftool: Type cftool in matlab comamnd line:
-%Coefficients (with 95% confidence bounds):
-a =       129.9 ;% (100.1, 159.8)
-b =       0.3666 ;% (0.3092, 0.4241)
-c =      -96.73 ;% (-127.8, -65.62)
-Thrust_to_throttle_func = a.*Thrust_avg.^b+c;
-% check other numbers:
-t1= a*0^b+c
-t2 = a*14^b+c
-%%
-% to save in diffrent folder saveas(gcf,[pwd
-% ['Motor1_114_Current' ],'.fig']); and
-% saveas(gcf,[pwd ['Motor1_120_Current' ],'.eps'],'epsc2');
-figure
-AxesFont_f=16;font_f=16;
-set(gcf,'DefaultAxesFontSize',AxesFont_f);
-
-Power_avg=Volt_avg.*Current_avg;
-scatter(Command_avg,Power_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Power_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148Powerfig');
-saveas(gcf,'C_tau_148Power','epsc2');
-
-scatter(Command_avg,Current_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Current_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148Currentfig');
-saveas(gcf,'C_tau_148Current','epsc2');
-figure
-scatter(Command_avg,Temp_avg)
-xlabel('Command_avg');ylabel('Temp_avg')
-saveas(gcf,'C_tau_148Temp.fig');
-saveas(gcf,'C_tau_148Temp.eps','epsc2');
-figure
-scatter(Command_avg,RPM_Hz_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('RPM_Hz_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148RPM_Hz.fig');
-saveas(gcf,'C_tau_148RPM_Hz.eps','epsc2');
-figure
-scatter(Command_avg,RPM_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('RPM_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148RPM.fig');
-saveas(gcf,'C_tau_148RPM.eps','epsc2');
-figure
-scatter(Command_avg,MAH_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('MAH_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148MAH.fig');
-saveas(gcf,'C_tau_148MAH.eps','epsc2');
-figure
-scatter(Command_avg,Volt_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Volt_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148Volt.fig');
-saveas(gcf,'C_tau_148Volt.eps','epsc2');
-figure
-scatter(Command_avg,Weight_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Weight_avg','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148wieght.fig');
-saveas(gcf,'C_tau_148wieght.eps','epsc2');
-figure
-scatter(Command_avg,Thrust_avg)
-xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
-ylabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex')
-saveas(gcf,'C_tau_148thrust.fig');
-saveas(gcf,'C_tau_148thrust.eps','epsc2');
-%% saving the requied data to find C_tau
-save('thrusts_data.mat','Command_avg','Thrust_avg')
+% % Now find the inverse of 2nd order polynomial:
+% Command_avg_inversepoly2=sqrt((Thrust_avg-p3)/p1+p2^2/4/p1/p1)-p2/2/p1;
+% 
+% figure
+% plot(Thrust_avg,Command_avg_inversepoly2,'--',Thrust_avg,Command_avg,'k.');
+% xlabel('Thrust (N)')
+% ylabel('Throttle')
+% title('throttle=sqrt((Thrust-p3)/p1+p2^2/4/p1/p1)-p2/2/p1')
+% 
+% %>>>>>>>>>>>>>use "cftool" for the curve fitting <<<<<< 
+% % from cftool: Type cftool in matlab comamnd line:
+% %Coefficients (with 95% confidence bounds):
+% a =       129.9 ;% (100.1, 159.8)
+% b =       0.3666 ;% (0.3092, 0.4241)
+% c =      -96.73 ;% (-127.8, -65.62)
+% Thrust_to_throttle_func = a.*Thrust_avg.^b+c;
+% % check other numbers:
+% t1= a*0^b+c
+% t2 = a*14^b+c
+% %%
+% % to save in diffrent folder saveas(gcf,[pwd
+% % ['Motor1_114_Current' ],'.fig']); and
+% % saveas(gcf,[pwd ['Motor1_120_Current' ],'.eps'],'epsc2');
+% figure
+% AxesFont_f=16;font_f=16;
+% set(gcf,'DefaultAxesFontSize',AxesFont_f);
+% 
+% Power_avg=Volt_avg.*Current_avg;
+% scatter(Command_avg,Power_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Power_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148Powerfig');
+% saveas(gcf,'C_tau_148Power','epsc2');
+% 
+% scatter(Command_avg,Current_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Current_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148Currentfig');
+% saveas(gcf,'C_tau_148Current','epsc2');
+% figure
+% scatter(Command_avg,Temp_avg)
+% xlabel('Command_avg');ylabel('Temp_avg')
+% saveas(gcf,'C_tau_148Temp.fig');
+% saveas(gcf,'C_tau_148Temp.eps','epsc2');
+% figure
+% scatter(Command_avg,RPM_Hz_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('RPM_Hz_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148RPM_Hz.fig');
+% saveas(gcf,'C_tau_148RPM_Hz.eps','epsc2');
+% figure
+% scatter(Command_avg,RPM_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('RPM_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148RPM.fig');
+% saveas(gcf,'C_tau_148RPM.eps','epsc2');
+% figure
+% scatter(Command_avg,MAH_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('MAH_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148MAH.fig');
+% saveas(gcf,'C_tau_148MAH.eps','epsc2');
+% figure
+% scatter(Command_avg,Volt_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Volt_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148Volt.fig');
+% saveas(gcf,'C_tau_148Volt.eps','epsc2');
+% figure
+% scatter(Command_avg,Weight_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Weight_avg','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148wieght.fig');
+% saveas(gcf,'C_tau_148wieght.eps','epsc2');
+% figure
+% scatter(Command_avg,Thrust_avg)
+% xlabel('Command_avg','fontsize',font_f,'interpreter','latex');
+% ylabel('Thrust_avg (N)','fontsize',font_f,'interpreter','latex')
+% saveas(gcf,'C_tau_148thrust.fig');
+% saveas(gcf,'C_tau_148thrust.eps','epsc2');
+% %% saving the requied data to find C_tau
+% save('thrusts_data.mat','Command_avg','Thrust_avg')
