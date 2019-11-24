43bb09d43ebcc2d5fe5bb403aa885f495a3a2598
Kani-FDCL <kanishkegb@gwu.edu>
2019-11-24 17:33:06 -0500

    add motor calibration for MN3110 motor and APC 10x4.7 at 16.2 Volts

--- python_scripts/record.py
+++ python_scripts/record.py
@@ -2,11 +2,12 @@
 This code records serial data as an array of strings
 """
 
+from __future__ import print_function
 import serial
 
 connected = False
 
-location='/dev/cu.usbmodem1A12401'
+location='/dev/ttyACM0'
 
 ser = serial.Serial(location, 9600)
 
@@ -15,7 +16,7 @@ while not connected:
     serin = ser.read()
     connected = True
 
-text_file = open('../tiger_u3_apc10x4p7/motor_calib_16p4.txt', 'w') #name of target file
+text_file = open('../tiger_mn3110-17_T9545/motor_calib_16p4.txt', 'w') #name of target file
 #recommended file name format: <path><motor><input_voltage><type>.txt
 #(type: v1, v2, nothing, etc)
 
@@ -24,7 +25,8 @@ n = 1
 while True:
     if ser.inWaiting():
         x = ser.read()
-        text_file.write(str(x.decode('utf-8')))
+        # text_file.write(str(x.decode('utf-8')))
+        text_file.write(str(x))
         print(str(x.decode('utf-8')), end='')
 
         if x.decode('utf-8') == '\n':
